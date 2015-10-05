764996f84f90438ee65d6918d75472f53bdf4053
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-05 13:50:49 -0500

    write passing fizzbuzz in Ruby

--- rubykata.rb
+++ rubykata.rb
@@ -13,3 +13,19 @@ def age_check(age)
 end
 
 age_check(90)
+
+def fizzbuzz(max)
+  max.times do |n|
+    if (n % 15).zero?
+      puts "fizzbuzz"
+    elsif (n % 3).zero?
+      puts "fizz"
+    elsif (n % 5).zero?
+      puts "buzz"
+    else
+        puts n
+    end
+  end
+end
+
+fizzbuzz(100)
