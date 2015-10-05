83b0417acf226c8ef6bf6d6d1b2b90d18e99c721
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-05 15:17:54 -0500

    write passing ageCheck in JS

--- kata.js
+++ kata.js
@@ -3,3 +3,21 @@ var print = function () {
 };
 
 print ()
+
+
+var age = age;
+function ageCheck(age) {
+  if (age > 21) {
+    console.log("proceed");
+  } else {
+    console.log("too young");
+    };
+}
+
+ageCheck(35);
+
+
+function fizzBuzz(max) {
+ max.(times).
+;
+};
