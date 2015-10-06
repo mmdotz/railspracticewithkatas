547c2b0ffbdd65edab6c33c79d19b36416201952
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-06 08:59:07 -0500

    write JS fizzbuzz passing

--- kata.js
+++ kata.js
@@ -14,20 +14,19 @@ function ageCheck(age) {
 }
 ageCheck(35);
 
-//fizzbuzz
-function fizzBuzz(max) {
 
-  [1..max].forEach(function(n) {
-    if ((n % 15) === 0) {
-      console.log("fizzbuzz");
-    } else if ((n % 3) === 0) {
-      console.log("fizz");
-    } else if ((n % 5) === 0) {
-      console.log("buzz");
-    } else {
-      console.log(n);
+function fizzBuzz(max) {
+for (var n = 1; n < max; n+=1) {
+  if ((n % 15) === 0) {
+    console.log("fizzbuzz");
+  } else if ((n % 3) === 0) {
+    console.log("fizz");
+  } else if ((n % 5) === 0) {
+    console.log("buzz");
+  } else {
+    console.log(n);
     };
-  });
+  };
 };
 
-fizzBuzz(75);
+fizzBuzz(75)
